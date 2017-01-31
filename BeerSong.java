1aaf41fcb201923c47b61a6cd22f33729b61f47e
btseiber <btseiber@oakland.edu>
2017-01-30 22:15:15 -0500

    Update BeerSong.java
    Code Added

--- BeerSong.java
+++ BeerSong.java
@@ -1,4 +1,13 @@
-Main ()
+Import java.util.Scanner;
 
-
-Ninety_Nine_Bottles_of_Beer()
+public class 99Bottles
+{
+public static void main(String[] args)
+{
+int a, t=99;
+for (int i=0; i<a; i++)
+{
+System.out.println ((t-i)+ " bottles of beer on the wall, " + (t-i) + " bottles of beer.");
+System.out.println ("take one down, pass it around. " + (t-i-1) + " bottles of beer on the wall.";
+}
+}
